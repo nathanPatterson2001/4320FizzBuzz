897936bfd730bb5f58329d54e1a8566cfe145e57
nathanPatterson2001 <njpcdc@mail.missouri.edu>
2022-10-14 15:08:26 -0500

    comments explaining program

--- 4320FizzBuzz1.py
+++ 4320FizzBuzz1.py
@@ -1,10 +1,14 @@
+#loop for the program 
 for fizzbuzz in range(100):
+#if the number can be divided by 3 and 5 print fizzbuzz
     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
         print("fizzbuzz")
         continue
+#if the number can be divided by 3 print fizz
     elif fizzbuzz % 3 == 0:
         print("fizz")
         continue
+#if the number can be divided by 5 print buzz
     elif fizzbuzz % 5 == 0:
         print("buzz")
         continue
