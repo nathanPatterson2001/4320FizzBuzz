5a8d4af8d907a2a7307d58febf62829b2d615110
nathanPatterson2001 <njpcdc@mail.missouri.edu>
2022-10-14 15:28:42 -0500

    Update 4320FizzBuzz2.py

--- 4320FizzBuzz2.py
+++ 4320FizzBuzz2.py
@@ -3,11 +3,14 @@
 for num in range(1,101):
 #string is the number that the output will have in the range
     string = ""
-
+#If the number can be divided by 5 print Buzz
     if num % 5 == 0:
         string = string + "Buzz"
+#If the number can be divided by 3 print Fizz
     if num % 3 == 0:
         string = string + "Fizz"
+#If the number can be divided by 3 and 5 FizzBuzz
+#Adding the strings together to made sure that the output is FizzBuzz
     if num % 5 != 0 and num % 3 != 0:
         string = string + str(num)
     print(string)
